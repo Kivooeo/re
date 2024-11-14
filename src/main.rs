4488f8aaf935ea3480d8eda52897c622096afabe
Kivooeo <Kivoeoo123@gmail.com>
2024-11-15 00:43:17 +0500

    init3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![deny(warnings)]
+
 use std::net::SocketAddr;
 
 use bytes::Bytes;
@@ -8,16 +10,20 @@ use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Method, Request, Response, Result, StatusCode};
 use hyper_util::rt::TokioIo;
-use pretty_env_logger;
 use tokio::{fs::File, net::TcpListener};
 use tokio_util::io::ReaderStream;
+
+static NOTFOUND: &[u8] = b"Not Found";
 
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
-    loop {}
-    let addr = "127.0.0.1:3000".parse::<SocketAddr>().unwrap();
+
+    let addr: SocketAddr = "0.0.0.0:1337".parse().unwrap();
     let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+    println!("{:?}", std::env::current_dir());
+
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
@@ -27,22 +33,54 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                 .serve_connection(io, service_fn(response_examples))
                 .await
             {
-                println!("{err:?}");
+                println!("Failed to serve connection: {:?}", err);
             }
         });
     }
 }
 
-// TODO*: Write a file send logic
 async fn response_examples(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") | (&Method::GET, "/index.html") => simple_file_send(INDEX).await,
-        (&Method::GET, "/no_file.html") => {
-            // Test what happens when file cannot be found
-            simple_file_send("this_file_should_not_exist.html").await
-        }
+        (&Method::GET, a) => simple_file_send(&a[1..]).await,
+
         _ => Ok(not_found()),
     }
+}
+
+/// HTTP status code 404
+fn not_found() -> Response<BoxBody<Bytes, std::io::Error>> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Full::new(NOTFOUND.into()).map_err(|e| match e {}).boxed())
+        .unwrap()
+}
+
+async fn simple_file_send(filename: &str) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
+    // Open file for reading
+    let file = File::open(format!("C:/Users/Ada/.shared/{}", filename)).await;
+    println!("{:?}", filename);
+    if file.is_err() {
+        eprintln!("ERROR: Unable to open file.");
+
+        return Ok(not_found());
+    }
+
+    let file: File = file.unwrap();
+
+    // Wrap to a tokio_util::io::ReaderStream
+    let reader_stream = ReaderStream::new(file);
+
+    // Convert to http_body_util::BoxBody
+    let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
+    let boxed_body = stream_body.boxed();
+
+    // Send response
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .body(boxed_body)
+        .unwrap();
+
+    Ok(response)
 }
