cb4a3d15c379218d8cde72cc464db8a54fcca127
Kivooeo <Kivooeo123@gmail.com>
2025-02-08 00:32:08 +0500

    few fixes check changelog

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ static NOTFOUND: &[u8] = b"Not Found";
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     let _ = dbg!(std::env::current_dir());
-    traverse(std::path::Path::new("/"), 0);
+    traverse(std::path::Path::new("/app"), 0);
 
 
     pretty_env_logger::init();
